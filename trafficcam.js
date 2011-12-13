5064a0ee501d27fc513c6387acf819ea4afb69af
Brad Hafichuk <brad@hafichuk.net>
2011-12-13 09:30:19 -0700

    initial checkin

---
+++ trafficcam.js
@@ -0,0 +1,30 @@
+javascript:(function(){
+    var newWindow = window.open('','Calgary Traffic Cams');
+    newWindow.focus();
+
+    // City of Calgary currently has 79 (somewhat) active traffic cameras
+    var numImages = 79;
+    var baseUrl = 'http://trafficcam.calgary.ca/loc%d.jpg';
+
+    // Adds the image tags to the page
+    function addImages() {
+
+	// Clear the contents of this page in case that someone re-runs this script (i.e. if there already is a 'Calgary Traffic Cams' page)
+	newWindow.document.body.innerHTML = '';
+
+	for (i = 0; i <= numImages; i++) {
+	    imgUrl = baseUrl.replace(/%d/, i);
+	    newWindow.document.write('<img id="cam_' + i + '" style="float: left; margin: 5px;" src="' + imgUrl + '" /> ');
+	}
+    }
+
+    function refreshImages() {
+	for (i = 0; i <= numImages; i++) {
+	    imgUrl = baseUrl.replace(/%d/, i);
+	    newWindow.document.getElementById('cam_' + i).src = imgUrl;
+	}
+    }
+
+    addImages();
+    setInterval(refreshImages, 30000);
+})();
