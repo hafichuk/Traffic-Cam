c6a10c8993636f50f104d74bbc62eda3898347b0
Jesse Patching <jpatching@blacklinegps.com>
2011-12-13 09:53:59 -0700

    * Formatted code

--- trafficcam.js
+++ trafficcam.js
@@ -11,20 +11,20 @@ javascript:(function(){
     // Adds the image tags to the page
     function addImages() {
 
-	// Clear the contents of this page in case that someone re-runs this script (i.e. if there already is a 'Calgary Traffic Cams' page)
-	newWindow.document.body.innerHTML = '';
+        // Clear the contents of this page in case that someone re-runs this script (i.e. if there already is a 'Calgary Traffic Cams' page)
+        newWindow.document.body.innerHTML = '';
 
-	for (i = 0; i <= numImages; i++) {
-	    imgUrl = baseUrl.replace(/%d/, i);
-	    newWindow.document.write('<img id="cam_' + i + '" style="float: left; margin: 5px;" src="' + imgUrl + '" /> ');
-	}
+        for (i = 0; i <= numImages; i++) {
+            imgUrl = baseUrl.replace(/%d/, i);
+            newWindow.document.write('<img id="cam_' + i + '" style="float: left; margin: 5px;" src="' + imgUrl + '" /> ');
+        }
     }
 
     function refreshImages() {
-	for (i = 0; i <= numImages; i++) {
-	    imgUrl = baseUrl.replace(/%d/, i);
-	    newWindow.document.getElementById('cam_' + i).src = imgUrl;
-	}
+        for (i = 0; i <= numImages; i++) {
+            imgUrl = baseUrl.replace(/%d/, i);
+            newWindow.document.getElementById('cam_' + i).src = imgUrl;
+        }
     }
 
     addImages();
